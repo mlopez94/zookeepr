0d2d2bec6b4ca213d7b95e9bc3755a20157cc19b
mlopez <lopezmatthew87@gmail.com>
2021-06-25 17:46:10 -0400

    11.2.6

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const { animals } = require("./data/animals");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express(); // To instantiate the server
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -69,6 +74,15 @@ app.get('/api/animals/:id', (req, res) => {
       res.send(404);
     }
   });
+
+  app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    
+    console.log(req.body);
+    res.json(req.body);
+  });
+
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
