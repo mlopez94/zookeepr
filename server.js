ae23f0485b69fbe7bb56851f23f5c76503300c1f
mlopez <lopezmatthew87@gmail.com>
2021-06-25 14:55:13 -0400

    changing port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { animals } = require("./data/animals");
 const express = require("express");
-
+const PORT = process.env.PORT || 3001;
 const app = express(); // To instantiate the server
 
 
@@ -52,6 +52,6 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log("API server now on port 3001!");
-});
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
